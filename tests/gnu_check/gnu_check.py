6e91a98f6acb8cd2840efbc9a353747ca5c9cde8
Miles A Curry <currymiles@gmail.com>
2019-11-11 21:53:57 +0000

    Added test directory with stub tests

---
+++ tests/gnu_check/gnu_check.py
@@ -0,0 +1,13 @@
+import os
+
+
+class gnu_check(self):
+    test_name = "GNU Compiler Check"
+    test_description = "Check to see if there is a GNU compiler is on the machine and able to be" \
+                       " found by SMARTs"
+
+    nCPUs = 1
+    status = None
+
+    def run(self):
+        pass
