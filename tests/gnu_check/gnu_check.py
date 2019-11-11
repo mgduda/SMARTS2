7e1bfbda8f092cfdc47bba574d90ecf709850ee2
Miles A Curry <currymiles@gmail.com>
2019-11-11 22:31:24 +0000

    Fleshed out information for two basic tests

--- tests/gnu_check/gnu_check.py
+++ tests/gnu_check/gnu_check.py
@@ -1,6 +1,3 @@
-import os
-
-
 class gnu_check(self):
     test_name = "GNU Compiler Check"
     test_description = "Check to see if there is a GNU compiler is on the machine and able to be" \
---
+++ tests/intel_check/intel_check.py
@@ -0,0 +1,9 @@
+class intel_check(self):
+    test_name = "Intel Compiler Check"
+    test_description = "Check to see if there is a Intel compiler is on the machine and able to be" \
+                       " found by SMARTs"
+    nCPUs = 1
+    status = None
+
+    def run(self):
+        pass
