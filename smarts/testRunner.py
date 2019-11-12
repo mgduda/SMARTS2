fcb0e3901e79cf8ac59d8530827d17bc2b478fc8
Miles A Curry <currymiles@gmail.com>
2019-11-12 18:35:15 +0000

    Updated print format - Looks a little nicer now

--- smarts/testRunner.py
+++ smarts/testRunner.py
@@ -37,24 +37,54 @@ class TestManager:
 
     def list_tests(self, *args, **kwargs):
         # List all the available tests
-        print("TestManager: list_tests() - Listing tests found in", self.test_directory)
+        print("TestManager: list_tests() - Listing tests found in:", self.test_directory)
 
-        # TODO: Pretty print out the test_filenames and their descriptions using either a table or
-        # the python str.format() list and
-        # https://docs.python.org/3.8/library/string.html#formatspec
+        errors = []
 
-        print("    Test Name\t        Test Description")
+        import textwrap
+
+        print("")
+        print("{0:>13} {1:>19}".format(' Launch Name', '| Long Name'))
+        print("-" * 50)
         for tests in os.listdir(self.test_directory):
+
             if os.path.isdir(os.path.join(self.test_directory, tests)):
                 testFile = os.path.join(tests, tests).replace('/', '.')
-                #print("- Test:", tests, '    ', testFile)
-                mod = import_module(testFile)
-                try: 
-                    test = getattr(mod, tests)
-                    print("- ", testFile, '\t\t', test.test_description)
-                except:
-                    print("- ", tests, "\tCould not load test attributes")
+                runName = testFile
 
+                if '.' in runName:
+                    runName = testFile.split('.')[0]
+
+                colLen = 15
+
+                if (len(runName) > colLen):
+                    runName = runName[0:15]
+                    runName += '...'
+                else:
+                    # Pad the string with spaces
+                    runName += " " * (colLen - len(runName) + 3)
+
+                try:
+                    mod = import_module(testFile)
+                    try:
+                        test = getattr(mod, tests)
+                        print("- {0}\t{1}".format(runName, test.test_name))
+                    except:
+                        print("- {0}\t{1}".format(runName, "Could not load this test's attributes"))
+
+
+                except Exception as e:
+                    # TODO: Instead of printing this in line, make a list of the tests that could
+                    # not be loaded and then print them out at the end.
+                    # Make sure the list is a list of tests AND a list of the REASONS they weren't
+                    # able to be loaded!
+                    errors.append([testFile, e])
+
+        print("\n")
+        print("WARNING: The following tests could not be loaded:\n")
+        print("{0:<1} {1:>39}".format("Test Name", "Reason"))
+        for err in errors:
+            print("WARNING: {0:<1} {1:>60}".format(str(err[0]), str(err[1])))
 
         return 0
 
