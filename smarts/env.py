a40571ae4c5bc2cab9caac28b13b0c8c4baba9ec
Miles A Curry <currymiles@gmail.com>
2019-11-12 18:50:46 +0000

    Updated end.py with more environment stubs

--- smarts/env.py
+++ smarts/env.py
@@ -9,6 +9,27 @@ class Environment:
         self.env = None
         return
 
+
+    """
+    TODO: Do checking of the env.yaml file:
+        
+    Check for both: 
+        - Description - Produces error
+        - Modests - Produce Warnings
+
+    Description Checks:
+        - Check for max_cores
+        - If modules = True then LMOD_CMD must be set
+        - If HPC = PBS then check there isn't slurm_options and visa versa
+        - If HPC = False and HPC_OPTIONS is present produce warning
+        - 
+    Modests:
+        - If a module command is listed, then check to see if the LMOD_CMD
+          is present in the description
+        - Libraries (if present) - Check to see if they are either modules (w/ optional version) or
+          either a ENV_NAME AND value pair.
+    """
+
     def parse_file(self):
         if not os.path.isfile(self.envFile):
             print("ERROR: '", self.envFile, "' does not exist!")
@@ -27,6 +48,31 @@ class Environment:
         print("Environment file read successfully!\n\n")
         return 0;
 
-    def list_modests(self):
-        # List all the modests that are in the env specification
+    def list_modests(self, name=None, *args, **kwargs):
+        # If name is None, list out all modests found in the environment.yaml file,
+        # else, list out the modests that contain name.
+        # Returns a list
         pass
+
+    def list_modset(self, modset, *args, **kwargs):
+        # List the information of a single modest
+        # This should probably return the yaml dictiony of said modset
+        pass
+
+    def list_libraries(self, modset, *args, **kwargs):
+        # List the libraries of a specifiec modset
+        # Returns a list
+        pass
+
+    def _load_compiler(self, compiler, executables, *args, **kwargs):
+        # Internal function to load a compiler on a modset
+        pass
+
+    def _load_library(self, library, *args, **kwargs):
+        # Internal function to load a library based on the modset lib
+        pass
+
+    def load_modest(self, modset, *args, **kawrgs):
+        # Load a modset
+        pass
+
