c547cd66b84befc23364d9fa99e28c2709641aa4
Miles A Curry <currymiles@gmail.com>
2019-11-25 21:29:17 +0000

    Test runner returns a list and now takes in src directory

--- smarts/testRunner.py
+++ smarts/testRunner.py
@@ -4,30 +4,65 @@ import string
 import sys
 from importlib import import_module
 
-class TestRunner:
-    def __init__(self, test_directory, *args, **kwargs):
-        self.directory = test_directory
+# from smarts import HPC
+# import environment
 
-        print("TestRunner: Test directory is: ", self.directory)
+class TestRunner:
+    def __init__(self, test_directory, env, src, *args, **kwargs):
+        # The test directory is the directory that holds each test's implementation , not where
+        # they are ran.
+        self.directory = test_directory
+        self.env = env
+        self.src = src
+        _debug_ = 0
+
+        # Based on if the env we have is an HPC or not, initalize the HPC
+        if env.hpc == True:
+           # Initalize HPC
+            pass
+        else:
+           # HPC = None
+            pass
+
+        # At a Python's program startup, sys.path is set to PYTHONPATH (and not 
+        # the normal PATH variable). Similar to the PATH environment, it is the locations
+        # Python will search for importing Python modules.
+        # To find the user created tests, append it to the front.
+        if _debug_ > 0:
+            print("TestRunner: Test directory is: ", self.directory)
+
         sys.path.insert(0, self.directory)
 
         self.manager = TestManager(self.directory)
         self.scheduler = TestScheduler()
 
-    def list_tests(self, **kwargs):
+    def list_tests(self, *args, **kwargs):
         # List all tests
-        self.manager.list_tests()
-        return 0
+        return self.manager.list_tests()
 
-
-    def list_test(self, tests, **kwargs):
+    def list_test(self, tests, *args, **kwargs):
         # List a single test
         pass
 
-    def run_tests(self, tests, **kwargs):
+    def run_tests(self, tests, env, *args, **kwargs):
         # For each test:
         #  Use the test manager to check the given test
         #  If the test exists then run it using the Test Scheduler
+
+        # Check To see if all tests requested on this system 
+        # Check to see if the requested tests are avaliable on this sytem.
+        # If they are, also check to see if the maximum CPU's are not over
+        # the environments ammount
+        for test in tests:
+            # TODO: Might be worthwhile to try to find as many tests as possible and then return
+            # the all the test names that were not found
+            if self.manager.list_test(test) == None:
+                print("The test", test, "could not be found!")
+                return "The test "+test+" could not be found!"
+
+        
+        # Create the subdirectory of this regression test run and then move the cwd
+        # into that test directory`
         pass
 
 
@@ -36,64 +71,44 @@ class TestManager:
         self.test_directory = test_directory
 
     def list_tests(self, *args, **kwargs):
-        # List all the available tests
-        print("TestManager: list_tests() - Listing tests found in:", self.test_directory)
+        # Return a list all the valid and invalid tests
+        verbose = kwargs.get('verbose', 0)
 
-        errors = []
+        if verbose > 0:
+            print("TestManager: list_tests() - Listing tests found in:", self.test_directory)
 
-        import textwrap
-
-        print("")
-        print("{0:>13} {1:>19}".format(' Launch Name', '| Long Name'))
-        print("-" * 50)
+        valid_tests = []
+        invalid_tests = []
         for tests in os.listdir(self.test_directory):
-
             if os.path.isdir(os.path.join(self.test_directory, tests)):
+
                 testFile = os.path.join(tests, tests).replace('/', '.')
                 runName = testFile
 
+                # Translate the directory path into what Python wants for its import_module command
                 if '.' in runName:
                     runName = testFile.split('.')[0]
-
-                colLen = 15
-
-                if (len(runName) > colLen):
-                    runName = runName[0:15]
-                    runName += '...'
-                else:
-                    # Pad the string with spaces
-                    runName += " " * (colLen - len(runName) + 3)
 
                 try:
                     mod = import_module(testFile)
                     try:
                         test = getattr(mod, tests)
-                        print("- {0}\t{1}".format(runName, test.test_name))
+                        valid_tests.append([runName, test.test_name])
                     except:
-                        print("- {0}\t{1}".format(runName, "Could not load this test's attributes"))
-
-
+                        invalid_tests.append([runName, "Could not load this test's attributes"])
                 except Exception as e:
-                    # TODO: Instead of printing this in line, make a list of the tests that could
-                    # not be loaded and then print them out at the end.
-                    # Make sure the list is a list of tests AND a list of the REASONS they weren't
-                    # able to be loaded!
-                    errors.append([testFile, e])
+                    invalid_tests.append([testFile, e])
 
-        print("\n")
-        print("WARNING: The following tests could not be loaded:\n")
-        print("{0:<1} {1:>39}".format("Test Name", "Reason"))
-        for err in errors:
-            print("WARNING: {0:<1} {1:>60}".format(str(err[0]), str(err[1])))
-
-        return 0
+        return valid_tests, invalid_tests
 
     def list_test_suites(self, **kwargs):
         # List all of the available test-suites
         pass
 
     def list_test(self, name, **kwargs):
-        # Print the information about a single test or test-suite
+        # Return the information about a single test or test-suite as a (dictionary or object?)
+
+
         pass
 
     def check_test(self, name, **kwargs):
